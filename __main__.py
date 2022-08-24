b21eab1286c8ea988ab3ab330817447639e65572
Ylkhana <48254532+Ylkhana@users.noreply.github.com>
2022-08-24 11:10:17 +0200

    Added laser timer, laser_shoot update

--- __main__.py
+++ __main__.py
@@ -1,13 +1,37 @@
 import pygame, sys
 
 class Ship(pygame.sprite.Sprite):
-    def __init__(self, groups) -> None:
+    def __init__(self, groups):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/ship.png').convert_alpha()
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        self.can_shoot = True
+        self.shoot_time = None
+
+    def input_position(self):
+        pos = pygame.mouse.get_pos()
+        self.rect.center = pos
+
+    def laser_shoot(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            print('Pew pew')
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def shoot_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time > 250:
+                self.can_shoot = True
+        
+    def update(self):
+        self.input_position()
+        self.laser_shoot()
+        self.shoot_timer()
+
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, position, groups) -> None:
+    def __init__(self, position, groups):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/laser.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = position)
@@ -41,6 +65,9 @@ while True:
 
     # Background
     display_surface.blit(bg_image,(0,0))
+
+    # Update
+    spaceship_group.update()
 
     # Graphics
     spaceship_group.draw(display_surface)
